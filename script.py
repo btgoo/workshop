8363bd36fcf089df25e768248641792a6c84b457
“Otgonbayar <btgonbayr@gmail.com>
2025-07-05 10:23:22 +0800

    second commit

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+import psutil
+import os
+
+print("CPU usage (%):", psutil.cpu_percent(interval=1))
+current_dir_stats = psutil.disk_usage(os.getcwd())
+print(f"Total disk space: {current_dir_stats.total / (1024**3):.2f} GB")
+print(f"Used disk space: {current_dir_stats.used / (1024**3):.2f} GB")
+print(f"Free disk space: {current_dir_stats.free / (1024**3):.2f} GB")
+print(f"Percentage used: {current_dir_stats.percent}%")
